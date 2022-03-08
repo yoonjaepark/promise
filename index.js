6f12f06442cb72ed795ca277cb4fd45d1e04d725
yoonjaepark <rhkdro@gmail.com>
2022-03-08 21:32:05 +0900

    feat: Promise 속성 구조 설정

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Promise
+ * @author yoonjaepark
+ * @param {Function} fn
+ */
+function Promise(fn) {
+}
+
+Promise.resolve = function () {
+};
+
+Promise.reject = function () {
+};
+
+Promise.prototype.then = function () {
+};
+
+Promise.prototype.catch = function () {
+};
+
+Promise.prototype.finally = function () {
+};
+
+Promise.all = function () {
+};
+
+export {
+  Promise
+}
