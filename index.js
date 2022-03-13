76a4f58ec55f503f2e6e26d662e7ab0f0c6c614d
yoonjaepark <rhkdro@gmail.com>
2022-03-13 12:51:41 +0900

    feat: 코드 정리

--- index.js
+++ index.js
@@ -25,86 +25,35 @@ function Promise(fn) {
   this.promiseInstance = null;
   /** @type {any} */
   this.value = null;
-
+  
   fn(this.constructor.resolve.bind(this), this.constructor.reject.bind(this));
 }
 
 Promise.resolve = function (value) {
-  this.status = STATUS.FULFILLED;
-  this.value = value;
-  if (this.onFulfilledCallback) {
-    this.onFulfilledCallback(value);
-    if (this.promiseInstance.onFinallyCallback !== null) {
-      this.promiseInstance.onFinallyCallback(value);
-    }
-  }
+  handleCallback.call(this, STATUS.FULFILLED, value);
 };
 
 Promise.reject = function (value) {
-  this.status = STATUS.REJECTED;
-  this.value = value;
-  if (this.onRejectedCallback !== null) {
-    this.onRejectedCallback(value);
-  } else {
-    const instance = getLastInstance(this.promiseInstance, value);
-    instance.onRejectedCallback(value)
-    if (instance.onFinallyCallback !== null) {
-      instance.onFinallyCallback(value);
-    }
-  }
-};
-
-function getLastInstance(instance, value) {
-  if (instance.onRejectedCallback === null && instance.promiseInstance) {
-    return getLastInstance(instance.promiseInstance, value);
-  }
-  return instance
-}
-
-Promise.prototype.then = function (cb) {
-  this.promiseInstance = new Promise(function (resolve, reject) {
-    if (this.status === STATUS.PENDING) {
-      this.onFulfilledCallback = function () {
-        const result = cb(this.value);
-        if (result instanceof Promise) {
-          result.then(resolve, reject);
-        } else {
-          resolve(result);
-        }
-      }
-    }
-    if (this.status === STATUS.FULFILLED) {
-      cb(this.value);
-    }
-  }.bind(this))
-
-  return this.promiseInstance;
+  handleCallback.call(this, STATUS.REJECTED, value);
 };
 
 Promise.prototype.catch = function (cb) {
-  if (this.status === STATUS.PENDING) {
-    this.onRejectedCallback = cb;
-  }
-  if (this.status === STATUS.REJECTED) {
-    cb(this.value);
-  }
-  return this;
+  return handleMethod.call(this, 'catch', cb);
 };
 
 Promise.prototype.finally = function (cb) {
-  if (this.status === STATUS.PENDING) {
-    this.onFinallyCallback = cb;
-    return;
-  }
-  cb(this.value);
+  return handleMethod.call(this, 'finally', cb);
 };
-  
+
+Promise.prototype.then = function (cb) {
+  return handleMethod.call(this, 'then', cb);
+};
+
 Promise.all = function (arr) {
   return new Promise(function (resolve, reject) {
     const result = arr.map(() => ({
-      state: STATUS.PENDING,
+      state: STATUS.PENDING
     }));
-
     arr.forEach((iter, idx) => {
       if (iter instanceof Promise === false) {
         result[idx] = {
@@ -123,11 +72,84 @@ Promise.all = function (arr) {
         const isDone = !result.some(v => v.state === STATUS.PENDING);
         if (isDone) resolve(result.map(v => v.value));
       }).catch(err => {
-        reject(err)
+        reject(err);
       });
     });
   });
 };
+
+function getLastInstance(instance, value) {
+  if (instance.onRejectedCallback === null && instance.promiseInstance) {
+    return getLastInstance(instance.promiseInstance, value);
+  }
+  return instance;
+}
+
+function handleCallback(status, value) {
+  this.status = status;
+  this.value = value;
+
+  if (status === STATUS.FULFILLED) {
+    if (this.onFulfilledCallback) {
+      this.onFulfilledCallback(value);
+      if (this.promiseInstance.onFinallyCallback !== null) {
+        this.promiseInstance.onFinallyCallback(value);
+      }
+    }
+  }
+
+  if (status === STATUS.REJECTED) {
+    if (this.onRejectedCallback !== null) {
+      this.onRejectedCallback(value);
+    } else {
+      const instance = getLastInstance(this.promiseInstance, value);
+      instance.onRejectedCallback(value)
+      if (instance.onFinallyCallback !== null) {
+        instance.onFinallyCallback(value);
+      }
+    }
+  }
+}
+
+function handleMethod(method, cb) {
+  if (method === 'catch') {
+    if (this.status === STATUS.PENDING) {
+      this.onRejectedCallback = cb;
+    }
+    if (this.status === STATUS.REJECTED) {
+      cb(this.value);
+    }
+    return this;
+  }
+
+  if (method === 'finally') {
+    if (this.status === STATUS.PENDING) {
+      this.onFinallyCallback = cb;
+      return;
+    }
+    cb(this.value);
+  }
+
+  if (method === 'then') {
+    this.promiseInstance = new Promise(function (resolve, reject) {
+      if (this.status === STATUS.PENDING) {
+        this.onFulfilledCallback = function () {
+          const result = cb(this.value);
+          if (result instanceof Promise) {
+            result.then(resolve, reject);
+          } else {
+            resolve(result);
+          }
+        }
+      }
+      if (this.status === STATUS.FULFILLED) {
+        cb(this.value);
+      }
+    }.bind(this));
+
+    return this.promiseInstance;
+  }
+}
 
 export {
   Promise
